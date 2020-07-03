7b7e9bff325063c4c3caee395be79511963fd787
Chris Yoon <chy.yoon@gmail.com>
2020-07-02 21:49:23 -0700

    Make changes to the main.js functions and append the value to the html successfully

--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import $ from 'jquery';
 import { ExchangeService } from './exchangeservice';
 
 function showExchange(response) { 
-  if(response) {
-    $('#output').text('${response.conversion_rates.[0][0]');
+  // if(response) {
+    console.log(response);
+
+    $('#output').text(`Your return is $${response}`);
     $('#showErrors').text("");
-  }
-  else {
-    $('#showErrors').text("There was an error with your entry: ${Error.message}");
-    $('#output').text(""); 
-  } 
+  // }
+  // else {
+  //   $('#showErrors').text("There was an error with your entry: ${Error.message}");
+  //   $('#output').text(""); 
+  // } 
 }
 
 $(document).ready(function(){
