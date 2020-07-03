bfd4f78fb435294566f66e2773bb2087e0cd13ce
Chris Yoon <chy.yoon@gmail.com>
2020-07-02 18:24:30 -0700

    Add the ui and functionality to the click call back.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
+// import { PotterService } from './potterService.js';
+
+
 
 $(document).ready(function(){
-
+  $('#calculateExchange').click(function() {
+    (async () => {
+      let backendExchange = new BackendExchange();
+      const response = await backendExchange.getNewRate();
+      getHousePicture(response);
+    })();
 });
