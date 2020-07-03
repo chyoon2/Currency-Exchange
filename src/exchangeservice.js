8f133dc8d8021de2487c030aaacc9e80a6114fe7
Chris Yoon <chy.yoon@gmail.com>
2020-07-02 21:07:28 -0700

    it will add varaible assignments and navigation of json

---
+++ src/exchangeservice.js
@@ -0,0 +1,19 @@
+export class ExchangeService {
+  async getNewRate(amount, foreignCurrency) {
+    try {
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      let result = jsonifiedResponse.conversion_rates[foreignCurrency];
+      console.log(result);
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+}
