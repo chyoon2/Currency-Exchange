76102ff555c60b010d2fe4d791e322ce8e9e72fb
Chris Yoon <chy.yoon@gmail.com>
2020-07-02 19:10:00 -0700

    Add a showExchange UI function that will .text the output or an error

--- src/main.js
+++ src/main.js
@@ -2,15 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-// import { PotterService } from './potterService.js';
+import { BackendExhange } from './backendexchange.js';
 
-
+function showExchange(response) { 
+  if(response) {
+    $('#output').text('${response.conversion_rates.[0][0]');
+    $('#showErrors').text("");
+  }
+  else {
+    $('#showErrors').text("There was an error with your entry: ${Error.message}")
+    $('#output').text(""); 
+  } 
+}
 
 $(document).ready(function(){
   $('#calculateExchange').click(function() {
     (async () => {
       let backendExchange = new BackendExchange();
       const response = await backendExchange.getNewRate();
-      getHousePicture(response);
+      showExchage(response);
     })();
+  });
 });
